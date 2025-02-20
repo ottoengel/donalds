2b07eb7dee0d79ab7b005b431b022d1962cf0a87
Otto <ottoengelhardt18@gmail.com>
2025-02-19 21:55:39 -0300

    feat: add consumption method page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,30 @@
+import { db } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  return (
+    <h1>
+      {slug} {consumptionMethod}
+    </h1>
+  );
+};
+
+export default RestaurantMenuPage;
